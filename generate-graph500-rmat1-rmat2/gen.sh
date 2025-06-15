7c907bb377bc1e8dc9def8d372b41f740812a5c3
Pawel Balawender <pbalawender101@gmail.com>
2025-06-16 00:12:41 +0200

    fix

--- generate-graph500-rmat1-rmat2/gen.sh
+++ generate-graph500-rmat1-rmat2/gen.sh
@@ -1,40 +1,39 @@
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 1 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 2 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 3 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 8 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 1 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 2 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 3 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 4 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 8 . reuse
 
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 6 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 7 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 8 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 9 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 10 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 11 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 12 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 13 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 14 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 15 4 . reuse
-python3 graph500_convert_and_split.py ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 16 4 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 4 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 5 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 6 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 7 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 8 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 9 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 10 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 11 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 12 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 13 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 14 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 15 . reuse
 
-
-
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-12_4096_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-16_65536_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-7_128_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-13_8192_4'
 bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_1'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_8'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-8_256_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-10_1024_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-14_16384_4'
 bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_2'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-9_512_1'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-11_2048_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-15_32768_4'
 bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_3'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-6_64_4'
-bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-9_512_4'
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_4'
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_8'
 
+
+
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_4' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_5' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_6' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_7' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_8' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_9' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_10' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_11' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_12' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_13' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_14' . reuse
+bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-5_32_15' . reuse
