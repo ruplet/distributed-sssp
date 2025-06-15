7b7baaca1de30fe96bceafc5e7dc79c2654cdcff
Pawel Balawender <pbalawender101@gmail.com>
2025-06-15 17:56:39 +0200

    smaller

--- testing_env/run_tests_big.py
+++ testing_env/run_tests_big.py
@@ -26,7 +26,8 @@ def run_tests(break_on_fail, local):
                 continue
             print(f"Solution: {solution.name}")
             for test in Path("tests").iterdir():
-                if test.name not in ['bigcycle_4300400500_72']:
+                # if test.name not in ['bigcycle_4300400500_72']:
+                if test.name not in ['bigcycle_10100200_48']:
                     print(f"Skipping: {test.name}", flush=True)
                     continue
                 print(f"Running: {test.name}", flush=True)
