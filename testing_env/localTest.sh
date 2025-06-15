cb2a04114f466735c5dca8fa291d0060d01a4885
Pawel Balawender <pbalawender101@gmail.com>
2025-06-15 10:30:32 +0200

    add bigger test sscript

--- testing_env/localTest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-make -C LOGIN69 local
-mpirun -n 4 ./test_command.sh LOGIN69 path_20_4
--- testing_env/sbatch_run_tests_big.sh
+++ testing_env/sbatch_run_tests_big.sh
@@ -2,11 +2,9 @@
 #SBATCH --job-name sssp-tests
 #SBATCH --output output.txt
 #SBATCH --account "g101-2284"
-#SBATCH --nodes 1
 #SBATCH --cpus-per-task 1
 #SBATCH --ntasks-per-node 24
-#SBATCH --time 00:10:00
+#SBATCH --time 00:60:00
 
 module load common/python/3.11
-make -C LOGIN69
-srun -n 4 ./test_command.sh LOGIN69 path_20_4
+python3 run_tests_big.py
