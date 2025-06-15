8aedd7628ddce5e545052f6d171f16728b22b3c4
Pawel Balawender <pbalawender101@gmail.com>
2025-06-16 00:51:04 +0200

    split

--- generate-graph500-rmat1-rmat2/gen.sh
+++ generate-graph500-rmat1-rmat2/gen.sh
@@ -1,22 +1,3 @@
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 1 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 2 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 3 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 4 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 8 . reuse
-
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 4 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 5 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 6 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 7 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 8 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 9 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 10 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 11 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 12 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 13 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 14 . reuse
-./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 15 . reuse
-
 bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_1'
 bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_2'
 bash -c '. ../generate-random-connected/.venv/bin/activate && python3 reference-dijkstra.py graph500-scale-3_8_3'
---
+++ generate-graph500-rmat1-rmat2/split.sh
@@ -0,0 +1,18 @@
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 1 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 2 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 3 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 4 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n3_ef1/ 3 8 . reuse
+
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 4 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 5 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 6 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 7 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 8 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 9 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 10 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 11 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 12 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 13 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 14 . reuse
+./split ~/graph500/src/out_a5700_bc1900_d10000_s2_3_n5_ef16/ 5 15 . reuse
