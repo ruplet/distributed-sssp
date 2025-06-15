e8c3d851fdef68c177cfc77218a2bd4d5ab54905
Pawel Balawender <pbalawender101@gmail.com>
2025-06-15 14:10:33 +0200

    fix

--- src/common.hpp
+++ src/common.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 // #define ENABLE_DELTA_STEPPING_OPTIMIZATIONS 1
-const bool ENABLE_DELTA_STEPPING_OPTIMIZATIONS = true;
+const bool ENABLE_DELTA_STEPPING_OPTIMIZATIONS = false;
 
 void logError(std::string msg) {
     std::cerr << "ERROR: " << msg << " (" << __FILE__ << ":" << __LINE__ << ")" << std::endl;
