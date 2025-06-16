a14320a5f653fd5ca9ef9844920e9af48d51f610
Pawel Balawender <pbalawender101@gmail.com>
2025-06-16 10:19:54 +0200

    more nodes

--- testing_env/sbatch_run_graph500.sh
+++ testing_env/sbatch_run_graph500.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name sssp-tests
 #SBATCH --output output.txt
 #SBATCH --account "g101-2284"
-#SBATCH --cpus-per-task 1
-#SBATCH --ntasks-per-node 12
-#SBATCH --time 02:00:00
+#SBATCH --ntasks-per-node 24
+#SBATCH --nodes 32
+#SBATCH --time 00:30:00
 
 module load common/python/3.11
 python3 run_graph500.py
