ffc4d88fc5703c3f96160ec77bea4a5ac2029fc1
Pawel Balawender <pbalawender101@gmail.com>
2025-06-16 11:34:44 +0200

    x

--- testing_env/sbatch_run_graph500.sh
+++ testing_env/sbatch_run_graph500.sh
@@ -4,7 +4,7 @@
 #SBATCH --account "g101-2284"
 #SBATCH --ntasks-per-node 12
 #SBATCH --nodes 64
-#SBATCH --time 00:08:00
+#SBATCH --time 00:12:00
 
 module load common/python/3.11
 python3 run_graph500.py
